b21016e1d29423d65d70d92c2466a5c6742da756
alekvan <aleksandar.vangelov@gmail.com>
2023-09-07 00:08:38 +0200

    fixed card image size

--- app/components/ArticleCard.tsx
+++ app/components/ArticleCard.tsx
@@ -4,6 +4,7 @@ export interface PetProps {
   id: number;
   petName: string;
   petColor: string;
+  petPictures: string[];
   petOwner: string;
   lgt: string;
   ltd: string;
@@ -13,14 +14,23 @@ export interface PetProps {
 
 const ArticleCard = ({ data }: { data: PetProps }) => {
   return (
-    <div className='w-pet-cards h-fit pb-6 mt-4 h-96 border-4 rounded-lg border-black'>
-      <Image
-        src='/images/front_page_news_bg.jpg'
-        alt='pet_image'
-        width={300}
-        height={200}
-        className='rounded-t-sm'
-      />
+    <div className='w-pet-cards h-fit pb-6 mt-4 border-2 rounded-lg border-black'>
+      <div className='w-full h-52 relative'>
+        {data.petPictures.map((imgUrl) => (
+          <Image
+            src={imgUrl}
+            alt='pet_image'
+            placeholder='empty'
+            loading='lazy'
+            key={imgUrl}
+            className='rounded-t-md'
+            fill
+            style={{
+              objectFit: 'cover',
+            }}
+          />
+        ))}
+      </div>
       <h2 className='flex text-3xl font-bold w-full justify-center my-2'>
         {data.petName}
       </h2>
